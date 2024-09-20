5943500ca7693ec5bee8d4e02f49e2cda7b14a40
1kkiren <1kkiren@mail.ru>
2024-09-20 13:14:10 +0300

    [Add] dataset

--- code/datasets/dataset.py
+++ code/datasets/dataset.py
@@ -10,6 +10,15 @@ os.environ["HF_HOME"] = "D:/.cache/huggingface"
 huggingface_hub.login(token=tokens['huggingface'])
 
 
+def change_labels(item):
+    if item['label'] == 0:
+        item['label'] = 2
+    elif item['label'] == 2:
+        item['label'] = 0
+
+    return item
+
+
 def main():
     russian_dataset = load_dataset(
         'ai-forever/kinopoisk-sentiment-classification')
@@ -22,22 +31,29 @@ def main():
     train_mixed = mixed_dataset['train']
     train_ru = train_ru.remove_columns(columns_to_drop_ru)
     train_mixed = train_mixed.remove_columns(columns_to_drop_mixed)
+    train_mixed = train_mixed.map(change_labels)
 
     val_ru = russian_dataset['validation']
     val_mixed = mixed_dataset['validation']
     val_ru = val_ru.remove_columns(columns_to_drop_ru)
     val_mixed = val_mixed.remove_columns(columns_to_drop_mixed)
+    val_mixed = val_mixed.map(change_labels)
 
     test_ru = russian_dataset['test']
     test_mixed = mixed_dataset['test']
     test_ru = test_ru.remove_columns(columns_to_drop_ru)
     test_mixed = test_mixed.remove_columns(columns_to_drop_mixed)
+    test_mixed = test_mixed.map(change_labels)
 
     class_labels = ClassLabel(names=["negative", "neutral", "positive"])
-    
+
     train_ru = train_ru.cast_column("label", class_labels)
     val_ru = val_ru.cast_column("label", class_labels)
     test_ru = test_ru.cast_column("label", class_labels)
+
+    train_mixed = train_mixed.cast_column("label", class_labels)
+    val_mixed = val_mixed.cast_column("label", class_labels)
+    test_mixed = test_mixed.cast_column("label", class_labels)
 
     train = concatenate_datasets([train_ru, train_mixed])
     val = concatenate_datasets([val_ru, val_mixed])
@@ -50,6 +66,7 @@ def main():
     })
 
     dataset.save_to_disk('datasets/multilingual-sentiments')
+    dataset.save_to_disk('code/datasets/multilingual-sentiments')
 
 
 if __name__ == "__main__":
